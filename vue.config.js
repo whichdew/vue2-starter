ddb8c089810dd11a16388849730afb3596031aa0
leoking <present150608@sina.com>
2022-07-06 00:08:59 +0800

    test: use promise in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+
+function doSomething(v){
+	return v?Promise.resolve():Promise.reject();
+}
+
+doSomething(1).then(r=>{
+	console.log(r)
+	module.exports=defineConfig({
+		devServer:{
+			port:8888
+		}
+	})
+}).catch(e=>{
+	console.error(e);
+	
 })
+
+//module.exports = defineConfig({
+//  transpileDependencies: true
+//})
